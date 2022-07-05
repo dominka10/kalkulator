5c118bcb146cd3a0d5467eb13377ba79483535c8
dominka10 <dt@gmail.com>
2022-07-05 12:49:55 +0200

    wynik

--- kalkulator.js
+++ kalkulator.js
@@ -1,10 +1,11 @@
 const wyswietlacz = document.querySelector(".wyswietlacz");
 const klawiatura = document.querySelector(".klawiatura");
+let kliknieteWartosci = [];
 // todo list:
 // 1. stworzenie klawiatury numerycznej
 //    -stworzenie przycisku
 //    - przypiesanie wartości do przycisku
-//    - obsługa kliknięcia przycisku (console.log)
+//    - obsługa kliknięcia przycisku (WstawWWyswietlaczu)
 //    - stworzenie cyfr (10) ( for each)
 //    - stworzenie operacji (4)
 // 2. stworzenie wyświetlacza
@@ -17,20 +18,48 @@ function StworzPrzycisk(wartosc, PoKlinieciu, klasa) {
   const przycisk = document.createElement("button");
   if (klasa) {
     przycisk.classList.add(klasa);
-  } // if robi klase opcjonalną (jeeli klasa nie jest pusta - to dodaj ją)
+  } // if robi klase opcjonalną (jezeli klasa nie jest pusta - to dodaj ją)
   przycisk.innerHTML = wartosc;
   przycisk.addEventListener("click", PoKlinieciu);
   klawiatura.append(przycisk);
 }
 function StworzKlawiature() {
   for (let wartosc = 0; wartosc < 10; wartosc++) {
-    StworzPrzycisk(wartosc, console.log, "cyfra");
+    StworzPrzycisk(wartosc, WstawWWyswietlaczu, "cyfra");
   }
-  StworzPrzycisk("+", console.log);
-  StworzPrzycisk("-", console.log);
-  StworzPrzycisk("=", console.log);
-  StworzPrzycisk("reset", console.log);
+  StworzPrzycisk("+", WstawWWyswietlaczu);
+  StworzPrzycisk("-", WstawWWyswietlaczu);
+  StworzPrzycisk("=", Wynik);
+  StworzPrzycisk("reset", Resetuj);
 }
-
+function Resetuj() {
+  kliknieteWartosci = [];
+  Wyswietl();
+}
+//
+function Wynik() {}
+function Wyswietl() {
+  wyswietlacz.textContent = kliknieteWartosci.join(""); // join wyswietla elementy w array bez przecinka
+}
+function WstawWWyswietlaczu(event) {
+  const wartosc = event.target.textContent;
+  // kliknieteWartosci[kliknieteWartosci.length] = wartosc; // to samo co push
+  if (wartosc === "+" || wartosc === "-") {
+    kliknieteWartosci.push(wartosc);
+  } else {
+    let ostatniElement = kliknieteWartosci[kliknieteWartosci.length - 1];
+    if (ostatniElement !== "+" && ostatniElement !== "-" && ostatniElement) {
+      kliknieteWartosci.pop();
+      kliknieteWartosci.push(parseInt(ostatniElement + wartosc, 10));
+    }
+    console.log(ostatniElement);
+    // if (ostatniElement == undefined) {
+    //   ostatniElement = 0;
+    // }
+  }
+  // tylko wtedy kiedy ostatni element nie jest pusty /  jesli jest pusty badz 0
+  Wyswietl();
+}
+Wyswietl();
 StworzKlawiature();
 // tu wywolac StworzPrzycisk tyle razy ile potrzeba
